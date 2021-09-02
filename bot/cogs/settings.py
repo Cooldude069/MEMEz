71ad81f6a04408a68f638e06bcce2f47cb00f120
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-09-02 12:24:49 +0530

    Starting work on settings command.

---
+++ bot/cogs/settings.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+ENABLED = "✓"
+DISABLED = "❌"
+
+
+class Settings(commands.Cog):
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
+
+    @commands.command(name="settings")
+    async def edit_settings(self, ctx, *kwargs):
+        print(kwargs)
+
+
+def setup(client: commands.Bot):
+    client.add_cog(Settings(client))
