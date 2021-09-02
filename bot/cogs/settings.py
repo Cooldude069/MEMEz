51b4cf5853098e7ca009ea95a924b305a8477049
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-09-02 12:34:33 +0530

    Testing argument parser.

--- bot/cogs/settings.py
+++ bot/cogs/settings.py
@@ -6,13 +6,23 @@ ENABLED = "✓"
 DISABLED = "❌"
 
 
+def arg_parser(*args) -> dict:
+    _args = {}
+    for arg in args:
+        if arg.startswith("--"):
+            _args[arg.lstrip("--")] = args[args.index(arg) + 1]
+
+    return _args
+
+
 class Settings(commands.Cog):
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
 
     @commands.command(name="settings")
-    async def edit_settings(self, ctx, **kwargs):
-        print(kwargs)
+    async def edit_settings(self, ctx, *args):
+        argvs = arg_parser(args)
+        await ctx.send(argvs)
 
 
 def setup(client: commands.Bot):
